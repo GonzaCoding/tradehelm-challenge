e068a694178dfe3f1c021e843e2760a2ab36fed9
GonzaCoding <gonzalo.iglesias@mercadolibre.com>
2021-01-26 19:52:26 -0300

    Item component and base styles

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 
+import "./styles.scss";
+
 import {ItemType} from "./types";
 
 interface Props {
   item: ItemType;
+  itemIndex: number;
+  onDelete: (index: number) => void;
 }
 
-const Item: React.FC<Props> = ({item}) => {
+const Item: React.FC<Props> = ({item, itemIndex, onDelete}) => {
   return (
     <div className="item">
       <p>{item.title}</p>
-      <p>{item.id}</p>
+      <button className="item-delete-button" onClick={() => onDelete(itemIndex)}>
+        <img
+          className="item-delete-button-image"
+          src="https://img.icons8.com/clouds/60/000000/delete-forever.png"
+        />
+      </button>
     </div>
   );
 };
