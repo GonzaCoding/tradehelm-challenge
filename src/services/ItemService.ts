7fa96fc61afc3988835c37babfb9354d46a05a31
GonzaCoding <gonzalo.iglesias@mercadolibre.com>
2021-01-26 19:52:04 -0300

    add ItemService

---
+++ src/services/ItemService.ts
@@ -0,0 +1,54 @@
+import {ItemType} from "../components/Item/types";
+
+export function addItem(item: string): Promise<ItemType> {
+  return new Promise<ItemType>((resolve, reject) => {
+    try {
+      const localItems: ItemType[] = JSON.parse(localStorage.getItem("itemList") as string) || [];
+
+      const newItem = {
+        title: item,
+      };
+
+      localItems.push(newItem);
+      localStorage.setItem("itemList", JSON.stringify(localItems));
+
+      setTimeout(() => {
+        resolve({
+          title: item,
+        });
+      }, 2000);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+export function getItems(): Promise<ItemType[]> {
+  return new Promise<ItemType[]>((resolve, reject) => {
+    try {
+      const localItems: ItemType[] = JSON.parse(localStorage.getItem("itemList") as string) || [];
+
+      resolve(localItems);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+export function deleteItem(index: number): Promise<ItemType[]> {
+  return new Promise<ItemType[]>((resolve, reject) => {
+    try {
+      const localItems: ItemType[] = JSON.parse(localStorage.getItem("itemList") as string) || [];
+
+      const newItems = localItems.filter((_, i) => i !== index);
+
+      localStorage.setItem("itemList", JSON.stringify(newItems));
+
+      setTimeout(() => {
+        resolve(newItems);
+      }, 2000);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
